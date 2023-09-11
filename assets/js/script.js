607eaf59555878b2ee30c487911d201c4402b866
Venancius <steniovenancius@gmail.com>
2023-09-11 14:56:21 -0300

    Setando o Last Name

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,15 @@
+
+// Constantes recebendo valores dos inputs
 const inputFirst = document.getElementById('firstName');
 const msgErroFirst = document.getElementById('erroFirtsName');
+const inputLast = document.getElementById('lastName');
+const msgErroLast = document.getElementById('erroLastName');
+const inputEmail = document.getElementById('email');
+const msgErroEmail = document.getElementById('erroEmail');
+const inputPassword = document.getElementById('password');
+const msgErroPassword = document.getElementById('erroPassword');
 
+// Setando funções
 function setInputFirst () {
     let inputNameFirst = inputFirst.value;
 
@@ -13,12 +22,33 @@ function setInputFirst () {
         msgErroFirst.style.display = 'none';
     }
 }
+function setInputLast () {
+    let inputNameLast = inputLast.value;
+
+    if (inputNameLast == ' ') {
+        inputLast.classList.add('inputErro');
+        inputLast.blur();
+        msgErroLast.style.display = 'flex';
+    } else if (inputNameLast != '') {
+        inputLast.classList.remove('inputErro');
+        msgErroLast.style.display = 'none';
+    }
+}
 
 
+// Eventos de caputura
 inputFirst.addEventListener("keyup", setInputFirst);
 inputFirst.addEventListener("focus", () => {
     inputFirst.setAttribute("placeholder", "");
 });
 inputFirst.addEventListener("blur", () => {
     inputFirst.setAttribute("placeholder", "First Name");
+});
+
+inputLast.addEventListener("keyup", setInputLast);
+inputLast.addEventListener("focus", () => {
+    inputLast.setAttribute("placeholder", "");
+});
+inputLast.addEventListener("blur", () => {
+    inputLast.setAttribute("placeholder", "Last Name");
 });
