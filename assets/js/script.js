98527305dbd633da8484d6536c1b34c771ff8ed4
Venancius <steniovenancius@gmail.com>
2023-09-22 14:58:46 -0300

    Finalização do projeto

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,47 +8,63 @@ const inputEmail = document.getElementById('email');
 const msgErroEmail = document.getElementById('erroEmail');
 const inputPassword = document.getElementById('password');
 const msgErroPassword = document.getElementById('erroPassword');
+const formulário = document.getElementById('form');
+const btnSubmit = document.getElementById('submit');
 
-// Setando funções
-function setInputFirst () {
-    let inputNameFirst = inputFirst.value;
+btnSubmit.addEventListener("click", (e) => {
+    // A validação só vai acontecer quando o botão for pressionado
+    let firstName = inputFirst.value.trim();
+    let lastName = inputLast.value.trim();
+    let email = inputEmail.value.trim();
+    let password = inputPassword.value.trim();
+    let validateEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    if (inputNameFirst == ' ') {
-        inputFirst.classList.add('inputErro');
-        inputFirst.blur();
+    e.preventDefault();
+
+    //First Name
+    if (firstName === '' ) {
+        inputFirst.classList.add("inputErro");
+        inputFirst.setAttribute("placeholder", " ");
         msgErroFirst.style.display = 'flex';
-    } else if (inputNameFirst != '') {
-        inputFirst.classList.remove('inputErro');
+    } else {
+        inputFirst.classList.remove("inputErro");
         msgErroFirst.style.display = 'none';
     }
-}
-function setInputLast () {
-    let inputNameLast = inputLast.value;
-
-    if (inputNameLast == ' ') {
-        inputLast.classList.add('inputErro');
-        inputLast.blur();
+    
+    //Last Name
+    if (lastName === '' ) {
+        inputLast.classList.add("inputErro");
+        inputLast.setAttribute("placeholder", " ");
         msgErroLast.style.display = 'flex';
-    } else if (inputNameLast != '') {
-        inputLast.classList.remove('inputErro');
+    } else {
+        inputLast.classList.remove("inputErro");
         msgErroLast.style.display = 'none';
     }
-}
 
+    //Email
+    if (email === '' ) {
+        inputEmail.classList.add("inputErro");
+        inputEmail.setAttribute("placeholder", " ");
+        msgErroEmail.style.display = 'flex';
+        msgErroEmail.textContent = "Email cannot be empty"
+    } else if(!email.match(validateEmail)) {
+        inputEmail.classList.add("inputErro");
+        inputEmail.style.color = "hsl(0, 100%, 74%)";
+        msgErroEmail.style.display = 'flex';
+    } else {
+        inputEmail.classList.remove("inputErro");
+        inputEmail.style.color = "hsl(0, 0%, 0%)";
+        msgErroEmail.style.display = 'none';
+        
+    }
 
-// Eventos de caputura
-inputFirst.addEventListener("keyup", setInputFirst);
-inputFirst.addEventListener("focus", () => {
-    inputFirst.setAttribute("placeholder", "");
-});
-inputFirst.addEventListener("blur", () => {
-    inputFirst.setAttribute("placeholder", "First Name");
-});
-
-inputLast.addEventListener("keyup", setInputLast);
-inputLast.addEventListener("focus", () => {
-    inputLast.setAttribute("placeholder", "");
-});
-inputLast.addEventListener("blur", () => {
-    inputLast.setAttribute("placeholder", "Last Name");
+    //Password 
+    if (password === '') {
+        inputPassword.classList.add("inputErro");
+        inputPassword.setAttribute("placeholder", " ");
+        msgErroPassword.style.display = 'flex';
+    } else {
+        inputPassword.classList.remove("inputErro");
+        msgErroPassword.style.display = 'none';
+    }
 });
